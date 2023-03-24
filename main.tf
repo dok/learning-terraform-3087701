adfc51e68158c98c8f6f433e827ed514f871ea10
dok <dokko.sean@gmail.com>
2023-03-24 14:53:12 -0700

    more vpc fixes

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ module "blog_sg" {
   version = "4.13.0"
   name = "blog_new"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -102,7 +102,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
