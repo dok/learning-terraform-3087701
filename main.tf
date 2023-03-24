6773bdd54afa24fb30d0533c51fbd027dfb89fb6
dok <dokko.sean@gmail.com>
2023-03-24 14:38:46 -0700

    fix blog_sg vpc_id

--- main.tf
+++ main.tf
@@ -49,7 +49,8 @@ module "blog_sg" {
   version = "4.13.0"
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
+
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
